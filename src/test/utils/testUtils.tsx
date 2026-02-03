db787aba4a7411bde724dda9e029485c225bdbbf
mggadea <matiasgaraygadea@outlook.com>
2026-02-03 19:28:35 -0300

    feat: add test

--- src/test/utils/testUtils.tsx
+++ src/test/utils/testUtils.tsx
@@ -2,7 +2,7 @@ import React, { type ReactElement } from 'react';
 import { render, type RenderOptions } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// Create a fresh QueryClient for each test
+
 const createTestQueryClient = () =>
   new QueryClient({
     defaultOptions: {
